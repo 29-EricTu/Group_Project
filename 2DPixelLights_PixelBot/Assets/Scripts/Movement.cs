e2ee44954732468c9e7b80b2663bbf8e3c8e3cfc
Kiryusz <155930031+Kiryusz@users.noreply.github.com>
2024-02-05 00:33:32 +0800

    Added Group4 Test

--- 2DPixelLights_PixelBot/Assets/Scripts/Movement.cs
+++ 2DPixelLights_PixelBot/Assets/Scripts/Movement.cs
@@ -9,7 +9,8 @@ public class Movement : MonoBehaviour
     ParticleSystem gun;
     Animator anim;
     public float speed = 3;
-
+    public string Direction;
+    public SpriteRenderer AHHHH;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +24,30 @@ public class Movement : MonoBehaviour
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
-        anim.SetFloat("x", x);
+        anim.SetFloat("Blend", x);
 
         transform.position += new Vector3(x * speed * Time.deltaTime, 0,0);
+        
+        if(Input.GetKeyDown(KeyCode.A)||(Input.GetKey(KeyCode.LeftArrow)))
+        {
+            AHHHH.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow)))
+        {
+            AHHHH.GetComponent<SpriteRenderer>().flipX = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-                gun.Play();
+            gun.Play();
+            anim.SetBool("IsShootBool", true);
+            speed = 0;
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
             gun.Stop();
+            anim.SetBool("IsShootBool", false);
+            speed = 3;
         }
     }
 }
---
+++ Group4 Test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float MovementSeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveRight()
+    {
+        rb.velocity = new Vector2(MovementSeed, rb.velocity.y);
+    }
+    public void MoveLeft()
+    {
+        rb.velocity = new Vector2(-MovementSeed, rb.velocity.y);
+    }
+}
